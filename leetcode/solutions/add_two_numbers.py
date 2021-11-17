b634bc3a35a8c0298fa3fea969abb5cc24a34558
rustamwho <rustamwho@gmail.com>
2021-11-17 14:08:49 +0300

    Small fix and add comments

--- leetcode/solutions/add_two_numbers.py
+++ leetcode/solutions/add_two_numbers.py
@@ -23,6 +23,7 @@ class ListNode:
 
 
 def str_from_linked_list(linked_list: ListNode):
+    """Return linked list in reverse string."""
     result = str(linked_list.val)
     while linked_list.next:
         linked_list = linked_list.next
@@ -30,15 +31,16 @@ def str_from_linked_list(linked_list: ListNode):
     return result[::-1]
 
 
-class Solution:
-    def add_two_numbers(self, l1: Optional[ListNode],
-                        l2: Optional[ListNode]) -> Optional[ListNode]:
-        l1 = int(str_from_linked_list(l1))
-        l2 = int(str_from_linked_list(l2))
-        result_linked_list = None
-        for x in str(l1 + l2):
-            result_linked_list = ListNode(x, result_linked_list)
-        return result_linked_list
+def add_two_numbers(l1: Optional[ListNode],
+                    l2: Optional[ListNode]) -> Optional[ListNode]:
+    l1 = int(str_from_linked_list(l1))
+    l2 = int(str_from_linked_list(l2))
+    result_linked_list = None
+
+    # Create new ListNode
+    for x in str(l1 + l2):
+        result_linked_list = ListNode(x, result_linked_list)
+    return result_linked_list
 
 
 if __name__ == '__main__':
@@ -51,5 +53,4 @@ if __name__ == '__main__':
     for x in l2_list[::-1]:
         input_l2 = ListNode(x, input_l2)
 
-    solution = Solution()
-    print(solution.add_two_numbers(input_l1, input_l2))
+    print(add_two_numbers(input_l1, input_l2))
